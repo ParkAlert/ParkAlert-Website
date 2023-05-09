daedfb083965016406ac0519183ec682af687f7e
keane90818 <keane9018@gmail.com>
2023-05-09 16:20:50 +0800

    gsap

--- pages/test.vue
+++ pages/test.vue
@@ -1,76 +1,85 @@
-<template>
-  <div class="container">
-    <div class="block -black"></div>
-    <div class="block -red"></div>
-  </div>
+<template class="bg-black">
+  <div class="container"></div>
+  <div class="car"><img src="../assets/car.png" class="carImg" /></div>
+  <div class="man"><img src="../assets/manWalk1.png" class="manImg" /></div>
+  <div class="phone"><img src="../assets/phone.png" class="phoneImg" /></div>
+  <div class="yellow"></div>
 </template>
 <script setup lang="ts">
 import gsap from "gsap";
 import { onMounted, onUnmounted, ref } from "vue";
-const Container = gsap.timeline();
 
 onMounted(() => {
-  // gsap.to(".box", {
-  //   x: 500,
-  //   y: 200,
-  //   scale: 0.3,
-  //   rotate: 360,
-  //   borderRadius: 50,
-  //   backgroundColor: "#ccc",
-  //   ease: "expo.out",
-  //   repeat: -1,
-  //   duration: 3
-  // });
-
-  const r1 = gsap
-    .timeline({ defaults: { ease: "none" } })
-    .to(".-red", { x: 100 })
-    .to(".-red", { y: 100 })
-    .to(".-red", { x: 200 })
-    .to(".-red", { y: 200 })
-    .duration(1);
-
-  // single Tween
-  const b1 = gsap.to(".-black", {
-    keyframes: [{ x: 100 }, { y: 100 }, { x: 200 }, { y: 200 }],
-    duration: 1
+  const runx = gsap.fromTo(".yellow", { width: 0 }, { width: 1350, duration: 3 });
+  const runy = gsap.fromTo(".yellow", { height: 5, y: 0 }, { height: 600, y: -310, duration: 3 });
+  const runCar = gsap.fromTo(".car", { x: 0 }, { x: 500, ease: "power1.inOut", duration: 3 });
+  const runMan = gsap.to(".man", { x: "-=580", duration: 3 });
+  const runPhone = gsap.to(".phone", { y: "+=200" });
+  const hugePhone = gsap.to(".phone", { ease: "power1.easeOut", scale: 100 });
+  const Container = gsap.timeline().add(runx).add(runy);
+  const imgMove = gsap.timeline().add(runCar).add(runMan).add(runPhone).add(hugePhone);
+  const hide = () => {
+    gsap.to(".yellow", { opacity: 0, duration: 1 });
+    gsap.to(".container", { opacity: 0, duration: 1 });
+  };
+  document.addEventListener("click", function () {
+    hide();
   });
-
-  const sum = gsap.timeline({ repeat: -1 }).add(r1).add(b1);
-
-  console.log(sum.getChildren());
-  // Container.to(".box", {
-  //   duration: 1,
-  //   x: 0,
-  //   y: 0,
-  //   scale: 2,
-  //   rotate: 360,
-  //   borderRadius: 0,
-  //   backgroundColor: "red",
-  //   ease: "expo.out",
-  //   repeat: 0
-  // });
-
-  //   gsap.to(".box", {
-  //     keyframes: [
-  //       { x: 100 }, // 0%
-  //       { y: 100 }, // 33.333%
-  //       { x: 200 }, // 66.666%
-  //       { y: 200 } // 100%
-  //     ]
-  //   });
-  // });
+  imgMove.then(() => {
+    // 動畫執行完畢，執行頁面跳轉
+    navigateTo("/");
+  });
 });
 </script>
 <style lang="scss">
-.block {
-  background: black;
-  width: 100px;
-  height: 100px;
-  border-radius: 10%;
-  margin-bottom: 20px;
-  &.-red {
-    background: red;
+.container {
+  position: absolute;
+  width: 100rem;
+  height: 100rem;
+  background-color: rgba(0, 0, 0, 0.5);
+  left: 0%;
+  top: 0%;
+  z-index: 1;
+}
+
+.yellow {
+  position: absolute;
+  top: 60%;
+  left: 0%;
+  background-color: yellow;
+  height: 10px;
+  width: 100rem;
+  z-index: 99;
+}
+
+.man {
+  .manImg {
+    width: 100px;
   }
+  position: absolute;
+  left: 95%;
+  top: 40%;
+  z-index: 100;
+}
+.car {
+  position: absolute;
+  left: 0%;
+  top: 40%;
+  width: 100px;
+  height: 10px;
+  width: 100rem;
+  z-index: 100;
+  .carImg {
+    width: 200px;
+  }
+}
+.phone {
+  .phoneImg {
+    width: 50px;
+  }
+  position: absolute;
+  z-index: 100;
+  left: 50%;
+  top: 0%;
 }
 </style>
